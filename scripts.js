88a97b17d2ba388898f5939325693feea44b5fe9
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2025-01-14 02:26:30 +0530

    Darken Pixels with each hover

--- scripts.js
+++ scripts.js
@@ -3,38 +3,54 @@ function log(message) {
     console.log(message);
 }
 
+const sketchspace = document.querySelector("div.sketchspace");
+
 function createGrid(num) {
     if (!(num < 100)) {
-        alert("Grid Size is too large! Keep it below 100.");
-    } else {
-        const sketchspace = document.querySelector("div.sketchspace");
-        // pixelSize is size of sketchspace divided by number of pixels
-        const pixelSize = 480.0 / num;
-        // total number of pixels is num * num
-        for (let i = 0; i < num * num; i++) {
-            let pixel = document.createElement("div");
-            pixel.classList.toggle("pixel");
-            pixel.setAttribute("style", `height: ${pixelSize}px; 
-                                width: ${pixelSize}px;`);
-            addListener(pixel);
-            sketchspace.appendChild(pixel);
-        }
-    }  
+        alert("Grid Size is too large! \
+Keep it below 100.\nDefault: 16X16 grid");
+        // Default grid is 16x16
+        num = 16;
+    } 
+    // pixelSize is size of sketchspace divided by number of pixels
+    const pixelSize = 480.0 / num;
+    // total number of pixels is num * num
+    for (let i = 0; i < num * num; i++) {
+        let pixel = document.createElement("div");
+        pixel.classList.toggle("pixel");
+        pixel.setAttribute("style", `height: ${pixelSize}px; 
+                            width: ${pixelSize}px;`);
+        randomizePixelColor(pixel);
+        increasePixelOpacity(pixel);
+        sketchspace.appendChild(pixel);
+        }  
 }
 
-function addListener(pixel) {
+function randomizePixelColor(pixel) {
+    let r_rgb = Math.round(Math.random()*256);
+    let b_rgb = Math.round(Math.random()*256);
+    let g_rgb = Math.round(Math.random()*256);
     pixel.addEventListener("mouseover", () => {
         // Randomize the color of pixels
-        r_rgb = Math.round(Math.random()*256);
-        b_rgb = Math.round(Math.random()*256);
-        g_rgb = Math.round(Math.random()*256);
         pixel.style["background-color"] = `rgb(${r_rgb}, ${g_rgb}, ${b_rgb})`;
     }); 
 }
 
+function increasePixelOpacity(pixel) {
+    let hoverCount = 1;
+    pixel.addEventListener("mouseover", () => {
+        // 11 is reached after 10 hovers
+        if (hoverCount == 11) {
+            pixel.style["background-color"] = "black";
+        } else {
+            // opacity can be set as a percentage
+            pixel.style["opacity"] = `${10 * hoverCount++}%`;
+        }
+    });
+}
+
 function changeGridSize() {
     // Removing previous grid first before adding new one
-    const sketchspace = document.querySelector("div.sketchspace");
     const pixels = document.querySelectorAll("div.pixel");
     pixels.forEach((pixel) => {
         sketchspace.removeChild(pixel);

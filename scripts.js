83ee03954b84071308641dc9daf401f765efcfdc
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2025-01-13 23:58:24 +0530

    Add functionality of Reset Button

--- scripts.js
+++ scripts.js
@@ -11,22 +11,35 @@ function addListener(pixel) {
 }
 
 function createGrid(num) {
-    const sketchspace = document.querySelector("div.sketchspace");
-    // pixelSize is size of sketchspace divided by number of pixels
-    const pixelSize = 480.0 / num;
-    // total number of pixels is num * num
-    for (let i = 0; i < num * num; i++) {
-        let pixel = document.createElement("div");
-        pixel.classList.toggle("pixel");
-        pixel.setAttribute("style", `height: ${pixelSize}px; 
-                            width: ${pixelSize}px;`);
-        pixel.addEventListener("mouseover", () => {
-            pixel.style["background-color"] = "black";
-        });
-        log(pixel.getAttribute("style"));
-        sketchspace.appendChild(pixel);
-    }
+    if (!(num < 100)) {
+        alert("Grid Size is too large! Keep it below 100.");
+    } else {
+        const sketchspace = document.querySelector("div.sketchspace");
+        // pixelSize is size of sketchspace divided by number of pixels
+        const pixelSize = 480.0 / num;
+        // total number of pixels is num * num
+        for (let i = 0; i < num * num; i++) {
+            let pixel = document.createElement("div");
+            pixel.classList.toggle("pixel");
+            pixel.setAttribute("style", `height: ${pixelSize}px; 
+                                width: ${pixelSize}px;`);
+            pixel.addEventListener("mouseover", () => {
+                pixel.style["background-color"] = "black";
+            });
+            sketchspace.appendChild(pixel);
+        }
+    }  
 }
 
-// const sketchspace() {}
-createGrid(16);
+const sizebtn = document.querySelector("button.sizebtn");
+const resetbtn = document.querySelector("button.resetbtn");
+let sizeOfGrid = parseInt(prompt("Enter the size of the grid: "));
+createGrid(sizeOfGrid);
+
+
+resetbtn.addEventListener("click", () => {
+    const pixels = document.querySelectorAll("div.pixel");
+    pixels.forEach((pixel) => {
+        pixel.style["background-color"] = "white";
+    });
+});
